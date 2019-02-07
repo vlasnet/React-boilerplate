60486bf196363c4932d89a046a157741337bef04
Vlasyuk Sergey <ma333day@gmail.com>
2019-02-07 11:05:03 +0200

    initial app commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const App = () => (
+  <div className={styles.app}>
+    <h1>Included modules examples:</h1>
+  </div>
+);
+
+export default App;
